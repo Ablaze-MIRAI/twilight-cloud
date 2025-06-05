7d6cda38900599ae3f82a22f8b345ab76647985d
nexryai <nexryai@sda1.net>
2025-06-05 01:43:04 +0000

    Breaking: postgresqlへの切り替え&Prismaでネイティブのクエリエンジンを使わないように

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,6 +1,13 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+
 import { PrismaClient } from "@prisma/client";
 
 class PrismaRepository extends PrismaClient {
+    constructor() {
+        const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+        super({ adapter });
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
