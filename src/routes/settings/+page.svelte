35a581d893d0e9857b43242ee3a7a5046c897b77
nexryai <61890205+nexryai@users.noreply.github.com>
2025-07-14 12:17:40 +0000

    WIP

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
-    import { IconDevicesCheck } from "@tabler/icons-svelte";
+    import { Button } from "@/lib/components/ui/button";
+    import type { User } from "@prisma/client";
+    import { IconKey, IconBrandGoogleFilled } from "@tabler/icons-svelte";
 
+    const linkGoogleAccount = async () => {
+        const res = await fetch("/api/me");
+        const user: User = await res.json();
+        document.cookie = `linkAccountId=${user.id}; SameSite=Strict; Secure`;
 
+        location.href = "/auth/google";
+    };
 </script>
 <div class="m-12">
     <h2 class="text-2xl">Settings</h2>
     <div class="m-6">
-        <h3 class="text-xl flex items-center"><IconDevicesCheck class="mr-3" /> Passkey</h3>
+        <h3 class="text-xl flex items-center"><IconKey class="mr-3" />Authenticate with External Accounts</h3>
+        <Button class="h-6" variant="outline" onclick={() => linkGoogleAccount()}>
+            <IconBrandGoogleFilled />
+            Google
+        </Button>
     </div>
 </div>
